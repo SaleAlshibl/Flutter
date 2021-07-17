e89cabe9cd7da6eb15366e00d683c1520dae57ce
SaleAlshibl <42993276+SaleAlshibl@users.noreply.github.com>
2021-07-17 16:47:18 +0300

    Create creatcalculatorpart1.dart

---
+++ creatcalculatorpart1.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyCalculator());
+
+
+class MyCalculator extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
+class Calculator extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _Calculator();
+  }
+}
+
+class _Calculator extends State<Calculator>{
+  String text = '0';
+  double numOne = 0;
+  double numTwo = 0;
+
+  String result = '0';
+  String finalResult = '0';
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
